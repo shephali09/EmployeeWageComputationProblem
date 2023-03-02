de000b126ee3a243ce7137940bf60633b49ff447
shephali09 <hiremathshephali9@gmail.com>
2023-03-02 13:06:21 +0530

    Add: Calculate daily Employee wage.

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,10 +1,14 @@
 echo "Welcome to Employee Wage Computation Program."
+
 isPresent=1;
 randomCheck=$((RANDOM%2));
 
 if [ $isPresent -eq $randomCheck ]
 then 
-echo "Employee is present"
+empRatePerHr=20
+empHrs=8
+salary=$(($empHrs*$empRatePerHr))
 else
-echo "Employee is absent"
+salary=0
 fi
+
