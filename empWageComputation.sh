5ad35b042c06cbe2a3240d1fc50642866934ad3c
shephali09 <hiremathshephali9@gmail.com>
2023-03-03 10:29:31 +0530

    Add: Calculate wages till a condition of total working hours or days is reached for a month - Assume 100 hours and 20 days

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,14 +1,17 @@
-echo "Welcome to Employee Wage Computation Program."
+echo "Welcome to Employee Wage Computation Program"
 
 isPartTime=1;
 isFullTime=2;
+empHrs=0;
 totalSalary=0;
+totalWorkingHour=0;
 empRatePerHr=20;
 numWorkingDays=20;
+day=1;
 
-for(( day=1; day<=$numWorkingDays; day++ ))
-
+while [[ $day -le 20 && $totalWorkingHour -lt 100 ]]
 do
+
 randomCheck=$((RANDOM%3));
 
 case $randomCheck in $isFullTime)
@@ -18,11 +21,17 @@ case $randomCheck in $isFullTime)
                      *)
                       empHrs=0;;
 esac
+totalWorkingHour=$(($totalWorkingHour+$empHrs))
 
-salary=$(($empHrs*$empRatePerHr))
+if [ $totalWorkingHour -gt 100 ]
+then
+totalWorkingHour=$(($totalWorkingHour-$empHrs))
+break;
+fi
+salary=$(($empRatePerHr*$empHrs))
 totalSalary=$(($totalSalary+$salary))
+((day++))
 
 done
 
-echo "Employee has earned $totalSalary$ in a month";
-
+echo "Employee has earned $totalSalary $ in a month (Total working Hour : $totalWorkingHour)";
