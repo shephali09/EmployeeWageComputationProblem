4425ec308b3671dfa39a289de19f0c29f484e0ac
shephali09 <hiremathshephali9@gmail.com>
2023-03-03 09:51:09 +0530

    Add: Calculating wages for a month - Assume 20 working days per month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,19 +1,28 @@
 echo "Welcome to Employee Wage Computation Program."
 
-isPartTime=1
-isFullTime=2
-empRatePerHr=20
-empCheck=$((RANDOM%3))
+isPartTime=1;
+isFullTime=2;
+totalSalary=0;
+empRatePerHr=20;
+numWorkingDays=20;
 
-case $empCheck in
-	$isFullTime)
-		empHrs=8 ;;
-	$isPartTime)
-		empHrs=4 ;;
-	*)
-	empHrs=0
+for(( day=1; day<=$numWorkingDays; day++ ))
+
+do
+randomCheck=$((RANDOM%3));
+
+case $randomCheck in $isFullTime)
+                      empHrs=8;;
+                     $isPartTime)
+                      empHrs=4;;
+                     *)
+                      empHrs=0;;
 esac
 
-salary=$((empHrs*$empRatePerHr))
+salary=$(($empHrs*$empRatePerHr))
+totalSalary=$(($totalSalary+$salary))
 
+done
+
+echo "Employee has earned $totalSalary$ in a month";
 
